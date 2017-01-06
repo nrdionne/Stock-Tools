10ee528d91f555a7184092fb41bbbc358e14291c
Nathan Dionne <nrdionne@gmail.com>
2017-01-05 22:10:53 -0500

    test

--- behavior/scripts/lib/algoliaClient.js
+++ behavior/scripts/lib/algoliaClient.js
@@ -2,6 +2,6 @@
 
 const algoliasearch = require('algoliasearch')
 
-exports.create = function create(VSW3HWXMZN, 3e9f0cd2fd07e3c9a57b3400e745c10f) {
-	return algoliasearch(VSW3HWXMZN, 3e9f0cd2fd07e3c9a57b3400e745c10f)
+exports.create = function create(a, secret) {
+	return algoliasearch(a, secret)
 }
